e3aa14172e3b8580843242ac511944898222430b
smsharma <smsharma@princeton.edu>
2018-11-10 11:50:20 -0500

    First commit

---
+++ SubhalosSims/units.py
@@ -0,0 +1,22 @@
+# Define units, with GeV as base unit
+GeV = 10**6;
+eV = 10**-9*GeV;
+KeV = 10**-6*GeV;
+MeV = 10**-3*GeV;
+TeV = 10**3*GeV;
+
+Sec = (1/(6.582119*10**-16))/eV; 
+Kmps = 3.3356*10**-6;
+Centimeter = 5.0677*10**13/GeV;
+Meter = 100*Centimeter;
+Km = 10**5*Centimeter;
+Kilogram = 5.6085*10**35*eV;
+Day = 86400*Sec;
+Year = 365*Day;
+KgDay = Kilogram*Day;
+amu = 1.66053892*10**-27*Kilogram;
+Mpc = 3.086*10**24*Centimeter;
+kpc = 1e-3*Mpc
+
+# Particle and astrophysics parameters
+M_s=1.99*10**30*(Kilogram)
