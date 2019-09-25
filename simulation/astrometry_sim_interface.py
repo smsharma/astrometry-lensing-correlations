7895ff62fd34d82ca92713288ec6c53011443f30
Siddharth Mishra Sharma <smsharma@feynman.princeton.edu>
2019-09-24 22:27:58 -0400

    Submitted run with no asymmetry

--- simulation/astrometry_sim_interface.py
+++ simulation/astrometry_sim_interface.py
@@ -24,8 +24,8 @@ sim = QuasarSim(max_sep=max_sep,
                 calc_powerspecs=True,
                 do_alpha=True,
                 save=True,
-                save_dir='/scratch/sm8383/QuasarSim',
-                save_tag='lcdm_nside128_altroll_sep20_mc' + str(imc))
+                save_dir='/mnt/hepheno/smsharma/QuasarSim',  # '/scratch/sm8383/QuasarSim',
+                save_tag='lcdm_nside128_noasymm_sep20_mc' + str(imc))
 
 sim.set_mass_distribution(sim.rho_M_SI, M_min=1e7*M_s, M_max=1e10*M_s, M_min_calib=1e8*M_s, M_max_calib=1e10*M_s, N_calib=150, alpha=-1.9)
 sim.set_radial_distribution(sim.r2rho_V_ein_EAQ, R_min=1e-3*kpc, R_max=260*kpc)
