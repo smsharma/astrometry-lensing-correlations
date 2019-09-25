457fff1285bdde5223149bdca9024747a1dd2e8f
Siddharth Mishra Sharma <smsharma@feynman.princeton.edu>
2019-09-24 22:27:58 -0400

    Submitted run with no asymmetry

--- simulation/cluster/submit.py
+++ simulation/cluster/submit.py
@@ -4,16 +4,25 @@ import random
 import numpy as np
 
 batch='''#!/bin/bash
-
 #SBATCH -N 1   # node count
 #SBATCH --ntasks-per-node=1
 #SBATCH -t 3:00:00
 #SBATCH --mem=4GB
+##SBATCH --mail-type=begin
+##SBATCH --mail-type=end
+##SBATCH --mail-user=sm8383@nyu.edu
+#SBATCH -p hepheno
+
+# MPI compilers
+export MKL_LP64_ILP64=ilp64
+source /opt/intel/compilers_and_libraries_2016.2.181/linux/bin/compilervars.sh intel64
+source /opt/intel/impi/5.0.3.048/bin64/mpivars.sh
 
 source ~/.bashrc
-conda activate
+source /group/hepheno/smsharma/heptools/anaconda3/etc/profile.d/conda.sh
+conda activate 
 
-cd /home/sm8383/Lensing-PowerSpectra/simulation
+cd /group/hepheno/smsharma/Lensing-PowerSpectra/simulation/
 
 '''
 
